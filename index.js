6d15cf20a8c51e8730fd58824dde74055afa4bb7
kriegesk <philipp@kriegeskorte.com>
2018-10-21 13:27:05 +0200

    Chat mit usernames
    so chat mit username usw tut jetzt :D nice !!! hoffe du kannst dran weiter arbeiten

--- index.js
+++ index.js
@@ -2,17 +2,47 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
+var usernames = [];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+	console.log('Socket verbunden');
+	
+	//Nachricht senden
+	socket.on('chat message', function(data){
+		io.emit('chat message', {msg: data,user: socket.username});
+		});
+	
+	socket.on('new user', function(data,callback){
+		if(usernames.indexOf(data) != -1){
+			callback(false);	
+		}else{
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+	
+	//Benutzernamen Updaten
+	function updateUsernames(){
+		io.emit('usernames', usernames);
+	}
+	
+	
+	//Disconnect
+	socket.on('disconnect',function(data){
+			if(!socket.username){
+				return;
+			}
+			usernames.splice(usernames.indexOf(socket.username),1);
+			updateUsernames();
+	});
 });
 
 http.listen(port, function(){
+  console.log('Server ist gestartet');
   console.log('listening on *:' + port);
 });
